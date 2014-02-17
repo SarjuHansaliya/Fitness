5305ba7eac2ee05fc9250d79b84f35068566ed44
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2014-02-17 10:56:35 +0530

    in between Edit reminder

---
+++ Fitness/ISEditReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ISEditReminderViewController.h
+//  Fitness
+//
+//  Created by ispluser on 2/17/14.
+//  Copyright (c) 2014 ISC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISEditReminderViewController : UIViewController
+
+@end
---
+++ Fitness/ISEditReminderViewController.m
@@ -0,0 +1,38 @@
+//
+//  ISEditReminderViewController.m
+//  Fitness
+//
+//  Created by ispluser on 2/17/14.
+//  Copyright (c) 2014 ISC. All rights reserved.
+//
+
+#import "ISEditReminderViewController.h"
+
+@interface ISEditReminderViewController ()
+
+@end
+
+@implementation ISEditReminderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
